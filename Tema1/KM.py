a6410f80d228d5de3d9c2807ad9012b7163b3f29
Haidau-Alina-Adriana <79049724+Haidau-Alina-Adriana@users.noreply.github.com>
2021-10-22 04:41:54 +0300

    Update KM.py

--- Tema1/KM.py
+++ Tema1/KM.py
@@ -1,48 +1,76 @@
 import socket
-import sys
-import time
-from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES
+from base64 import b64encode
 from os import urandom
+from base64 import b64decode
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+from Crypto.Random import get_random_bytes
 
-iv = '01020304050607080102030405060708'
+def generate_cbc_key(k, k_prim):
+    cipher = AES.new(k_prim, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(k, AES.block_size))
+    iv = b64encode(cipher.iv).decode('utf-8')
+    ct = b64encode(ct_bytes).decode('utf-8')
+    result = [iv, ct]
+    return result
 
-def generate_cbc_key(given_key):
-    secret_key = urandom(32)
-    cipher = AES.new(given_key.encode(), AES.MODE_CBC)
-    key = cipher.encrypt(secret_key)
-    return str(key)
+def dec_cbc(k_prim, iv, ct):
+    cipher = AES.new(k_prim, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(ct), AES.block_size)
 
-def generate_ecb_key(given_key):
-    secret_key = urandom(32)
-    cipher = AES.new(given_key.encode(), AES.MODE_ECB)
-    key = cipher.encrypt(secret_key)
-    return str(key)
+def generate_ecb_key(k, k_prim):
+    cipher = AES.new(k.encode(), AES.MODE_ECB)
+    return cipher.encrypt(k_prim)
 
-
-def realize_connection(HOST, PORT, given_key):
+def dec_ecb(k, message):
+    cipher = AES.new(k.encode(), AES.MODE_ECB)
+    return cipher.decrypt(message)
+    
+def realize_connection(HOST, PORT):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (HOST, PORT)
-    print("Conecting.. ")
     sock.connect(server_address)
-    # while True:
-    mode = sock.recv(1024)
-    mode = mode.decode('utf-8')
-    sock.send(str.encode(iv))
-    if mode == 'CBC':
-        key = generate_cbc_key(given_key)
-    elif mode == 'ECB':
-        key = generate_ecb_key(given_key)
-    sock.send(str.encode(key))
-        # print(data.decode('utf-8'))
-        # message = input("Enter message: ")
-        # sock.send(str.encode(message))
+    print("KM Connected. ")
+    
+    mode = sock.recv(4)
+    m = mode.decode('utf-8')
+    if m == 'CBC':
+        k_prim = b"alabalaportocala"
+        print("K_prim: ")
+        print(k_prim)
+        sock.send(k_prim)
+        k = get_random_bytes(16)
+        result = generate_cbc_key(k, k_prim)
+        iv = b64decode(result[0])
+        en_k = b64decode(result[1])
+        pt = dec_cbc(k_prim, iv, en_k)
+        print("K is:")
+        print(pt)
+        file = open('iv.txt', 'wb') 
+        file.write(iv)
+        # print("IV:")
+        # print(iv)
+        file.close()
+        # print("Key:")
+        # print(en_k)
+        sock.send(en_k)
+    elif m == 'ECB':
+        k_prim = 'alabalaportocala'
+        # print("K_prim: ")
+        # print(k_prim)
+        sock.send(k_prim.encode())
+        k = urandom(16)
+        en_k = generate_ecb_key(k_prim, k)
+        # print(en_k)
+        print("K is:")
+        pt  = dec_ecb(k_prim, en_k)
+        print(pt)
+        sock.send(en_k)
     sock.close()
 
 if __name__ == "__main__":
     HOST = '127.0.0.1'
-    PORT_KM = 8000
-    key = 'alabalaportocalaalabalaportocala' # de generat random cheia
-    realize_connection(HOST, PORT_KM, key)
-    
+    PORT_KM = 6000
+    realize_connection(HOST, PORT_KM)
     

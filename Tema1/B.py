be178aa019315297b70f0920b508f167275c5555
Haidau-Alina-Adriana <79049724+Haidau-Alina-Adriana@users.noreply.github.com>
2021-10-22 04:41:24 +0300

    Update B.py

--- Tema1/B.py
+++ Tema1/B.py
@@ -1,31 +1,60 @@
 import socket
-import sys
-import time
+from base64 import b64encode
+from os import urandom
+from base64 import b64decode
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+from Crypto.Random import get_random_bytes
+
+def dec_cbc(k_prim, iv, ct):
+    cipher = AES.new(k_prim, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(ct), AES.block_size)
+
+def dec_ecb(k, message):
+    cipher = AES.new(k.encode(), AES.MODE_ECB)
+    return cipher.decrypt(message)
 
 def realize_connection(HOST, PORT):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (HOST, PORT)
-    print("Conecting.. ")
+    print("B Connected.")
     sock.connect(server_address)
-    m = sock.recv(1024)
-    mode = m.decode('utf-8')
-    print("Mode: ")
-    print(mode)
-    k = sock.recv(1024)
-    key = k.decode('utf-8')
-    print("Key: ")
-    print(key)
-    # while True:
-        # print("Waiting for message: ")
-        # data = sock.recv(1024)
-        # print(data.decode('utf-8'))
-        # message = input("Enter message: ")
-        # sock.send(str.encode(message))
+    mode = sock.recv(4).decode('utf-8')
+    # print("Mode: ")
+    # print(mode)
+    if mode == 'ECB':
+        k_prim = sock.recv(16).decode('utf-8')
+        en_k = sock.recv(1024)
+        # print("K_prim: ")
+        # print(k_prim)
+        # print("Key: ")
+        # print(en_k)
+        print("K is:")
+        pt  = dec_ecb(k_prim, en_k)
+        print(pt)
+    elif mode == 'CBC':
+        file2 = open("iv.txt", 'rb')
+        iv = file2.read()
+        file2.close()
+        
+        k_prim = sock.recv(16)
+        # print("K_prim: ")
+        # print(k_prim)
+        # print("iv: ")
+        # print(iv)
+        en_k = sock.recv(1024)
+        # print("Key: ")
+        # print(en_k)
+        pt = dec_cbc(k_prim, iv, en_k)
+        print("K is:")
+        print(pt)
+   
     sock.close()
 
 if __name__ == "__main__":
     HOST = '127.0.0.1'
-    PORT_B = 5000
+    PORT_B = 4000
     realize_connection(HOST, PORT_B)
     
     
